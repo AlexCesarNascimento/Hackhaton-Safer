2e80a436c00c3d6e5298f767be8cafcb4fd4214b
bpcavalcante <brunoprofetacavalcante@gmail.com>
2020-10-10 12:58:59 -0300

    home fazendo

--- Safer/Safer/src/app/app-routing.module.ts
+++ Safer/Safer/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'navbar', component: NavbarComponent},
+  { path: 'footer', component: FooterComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
